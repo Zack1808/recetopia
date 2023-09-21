18443b46a382726832a4ede97f671a85e8d6c6ec
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-09-21 18:11:09 +0200

    Imported google fonts. Added and styled the logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 // Importing the style file
 import "./Navbar.css";
 
+// Importing the images
+import logo from "../../images/recetopia_logo.png";
+
 // Creating the Navbar component
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <h1>Recetopia</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // Exporting the component
