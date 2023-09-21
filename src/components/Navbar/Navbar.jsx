d61fcdf97d8ffa596ad869a9b56405bb65db8b0d
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-09-21 17:49:30 +0200

    Created the Navbar component and style file

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+// Importing the style file
+import "./Navbar.css";
+
+// Creating the Navbar component
+const Navbar = () => {
+  return <div className="navbar">Navbar</div>;
+};
+
+// Exporting the component
+export default Navbar;
