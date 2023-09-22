968f789543892087b0b25173605d0e9bcc909f83
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-09-22 14:55:07 +0200

    Implemented URL switching using programatic navigation

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,5 +1,11 @@
+// Importing the costume hooks
+import { useNavigate } from "../../context/navigation";
+
 // Creating the Link component
 const Link = ({ children, to, onClick, ...rest }) => {
+  // Getting the navigate function out of the context
+  const { navigate } = useNavigate();
+
   // Function that will prevent the defualt behaviour of the anchor tag
   const handleClick = (e) => {
     // Will prevent default behavior only if the control key or command key is not pressed
@@ -7,6 +13,9 @@ const Link = ({ children, to, onClick, ...rest }) => {
 
     // Will handle the onClick funciton if one was passed down
     onClick && onClick();
+
+    // Calling the funciton to programatically change the URL in the address bar
+    navigate(to);
   };
 
   return (
