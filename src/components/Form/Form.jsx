e9bb66052a3fac6ca26c473779a27b3b0e078c87
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-09-23 13:38:18 +0200

    Added the Form.css file. Styled the form element

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,25 @@
+import { forwardRef } from "react";
+
+// Importing the style file
+import "./Form.css";
+
 // Creating the Form component
-const Form = ({ children, onSubmit }) => {
+const Form = forwardRef(({ children, onSubmit }, ref) => {
   // Funciton that will handle the form submition
   const handleSubmit = (e) => {
+    // Prevents the default behaviour of the form
     e.preventDefault();
 
     // Will run the forwarded submition funciton if forwarded
-
     onSubmit && onSubmit();
   };
 
-  return <form onSubmit={handleSubmit}>{children}</form>;
-};
+  return (
+    <form ref={ref} onSubmit={handleSubmit}>
+      {children}
+    </form>
+  );
+});
 
 // Exporting the component
 export default Form;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 // Importing the style file
 import "./LoginPage.css";
