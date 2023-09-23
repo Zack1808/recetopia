7d1e9bd45351f23a6e35a9317386f09be632073f
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-09-23 14:19:31 +0200

    Added the LoginForm.css file. Styled the LoginForm component

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,6 +3,9 @@ import Form from "../Form/Form";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 
+// Importing the style file
+import "./LoginForm.css";
+
 // Creating the LoginForm component
 const LoginForm = () => {
   return (
@@ -10,9 +13,9 @@ const LoginForm = () => {
       <h2>Login</h2>
       <Form>
         <label htmlFor="email">Email</label>
-        <Input type="email" name="email" id="email" />
+        <Input type="email" name="email" id="email" required />
         <label htmlFor="password">Password</label>
-        <Input type="password" name="password" id="password" />
+        <Input type="password" name="password" id="password" required />
         <Button>Login</Button>
       </Form>
     </div>
