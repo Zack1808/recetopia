422abae51f65123d04fa78dd1e14853166a17409
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-09-23 11:46:56 +0200

    Added tabs to switch between the login and signup form

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,41 @@
+import { useState, useRef } from "react";
+
 // Importing the style file
 import "./LoginPage.css";
 
 // Creating the LoginPage componnet
 const LoginPage = () => {
+  // Setting up the state
+  const [tabNumber, setTabNumber] = useState(0); // State that will contain the information which tab is active
+
+  // Function that switches the tabs
+  const handleSwitch = (tabNum) => {
+    setTabNumber(tabNum);
+  };
+
   return (
     <div className="login-page">
-      <div className="container"></div>
+      <div className="container">
+        <div className="forms">
+          <div className="form-headers">
+            <h2
+              className={tabNumber === 0 ? "active" : ""}
+              onClick={() => handleSwitch(0)}
+            >
+              Login
+            </h2>
+            <h2
+              className={tabNumber === 1 ? "active" : ""}
+              onClick={() => handleSwitch(1)}
+            >
+              Sign Up
+            </h2>
+          </div>
+          <div className="form-body">
+            {tabNumber === 0 ? <h2>Login</h2> : <h2>Sign Up</h2>}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
