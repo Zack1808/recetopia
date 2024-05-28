6f5aad1b1ac1447289158a446269fe2a4c19c7c2
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-05-28 14:45:28 +0200

    Added ref forwarding

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,48 +4,53 @@ import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
 import { InputProps } from "../interfaces/components";
 
-const Input: React.FC<InputProps> = ({ type, title, required, ...rest }) => {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ type, title, required, ...rest }, ref) => {
+    const [isVisible, setIsVisible] = useState<boolean>(false);
 
-  const classes = classNames(
-    rest?.className,
-    "border-b-2 border-orange-300 rounded flex focus-within:ring-1 focus-within:ring-orange-300 focus-within:border-orange-500   transition"
-  );
+    const classes = classNames(
+      rest?.className,
+      "border-b-2 border-orange-300 rounded flex focus-within:ring-1 focus-within:ring-orange-300 focus-within:border-orange-500   transition"
+    );
 
-  const handleClickEvent = () => {
-    setIsVisible((prevState) => !prevState);
-  };
+    const handleClickEvent = () => {
+      setIsVisible((prevState) => !prevState);
+    };
 
-  return (
-    <div className="flex flex-col  gap-1">
-      {!!title && (
-        <label htmlFor={title} className="font-medium">
-          {title}{" "}
-          {required && <span className="font-semibold text-orange-500">*</span>}
-        </label>
-      )}
-
-      <div className={classes}>
-        <input
-          {...rest}
-          type={isVisible ? "text" : type}
-          id={title}
-          required={required}
-          className="py-2 px-3 flex-1 focus:outline-none"
-        />
-
-        {type === "password" && (
-          <button onClick={handleClickEvent} className="px-3">
-            {isVisible ? (
-              <FaEyeSlash className="text-orange-500" />
-            ) : (
-              <FaEye className="text-orange-500" />
+    return (
+      <div className="flex flex-col  gap-1">
+        {!!title && (
+          <label htmlFor={title} className="font-medium">
+            {title}{" "}
+            {required && (
+              <span className="font-semibold text-orange-500">*</span>
             )}
-          </button>
+          </label>
         )}
+
+        <div className={classes}>
+          <input
+            {...rest}
+            type={isVisible ? "text" : type}
+            id={title}
+            ref={ref}
+            required={required}
+            className="py-2 px-3 flex-1 focus:outline-none"
+          />
+
+          {type === "password" && (
+            <button onClick={handleClickEvent} className="px-3">
+              {isVisible ? (
+                <FaEyeSlash className="text-orange-500" />
+              ) : (
+                <FaEye className="text-orange-500" />
+              )}
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Input;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-import Input from "../components/Input";
-
 const Home: React.FC = () => {
-  return (
-    <div>
-      <Input type="password" placeholder="Text" title="title" required />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
