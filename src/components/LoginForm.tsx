c12c5039842894e16f52d40cb19cc84fe4b1c203
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-05-30 12:42:13 +0200

    Added Login functionality

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,28 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { ClipLoader } from "react-spinners";
 
 import Input from "./Input";
 import Button from "./Button";
 
+import { useAppDispatch } from "../hooks/storeHook";
+import { useLoginUser } from "../hooks/registrationHooks";
+
+import { LoginErrorState } from "../interfaces/states";
+
 const LoginForm: React.FC = () => {
+  const [errors, setErrors] = useState<LoginErrorState>({
+    errorUserName: false,
+    errorPassword: false,
+  });
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const dispatch = useAppDispatch();
+
+  const { loginUser } = useLoginUser({ setIsLoading, setErrors, dispatch });
+
+  const handleFormSubmition = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    loginUser(
+      formRef?.current?.Username?.value,
+      formRef?.current?.Password?.value
+    );
+  };
+
   return (
-    <form className="flex flex-col gap-5 items-end">
+    <form
+      className="flex flex-col gap-5 items-end"
+      ref={formRef}
+      onSubmit={handleFormSubmition}
+    >
       <Input
-        type="text"
-        placeholder="Cookmaster"
+        className={
+          errors.errorUserName ? "border-red-500 ring-1 ring-red-500" : ""
+        }
         title="Username"
+        type="text"
+        placeholder="CookMaster"
         required
         name="username"
+        onChange={() =>
+          setErrors((prevState) => ({ ...prevState, errorUserName: false }))
+        }
       />
 
       <Input
-        type="password"
-        placeholder="Password"
+        className={
+          errors.errorPassword ? "border-red-500 ring-1 ring-red-500" : ""
+        }
         title="Password"
+        type="password"
+        placeholder="password"
         required
         name="password"
+        onChange={() =>
+          setErrors((prevState) => ({ ...prevState, errorPassword: false }))
+        }
       />
 
       <Button
--- src/interfaces/hooks.ts
+++ src/interfaces/hooks.ts
@@ -1,10 +1,16 @@
 import React from "react";
 
-import { RegistrationErrorState } from "./states";
+import { RegistrationErrorState, LoginErrorState } from "./states";
 import { AppDispatch } from "../store";
 
 export interface UseRegisterUserProps {
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
   setErrors: React.Dispatch<React.SetStateAction<RegistrationErrorState>>;
+  dispatch: AppDispatch;
+}
+
+export interface useLoginUserProps {
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setErrors: React.Dispatch<React.SetStateAction<LoginErrorState>>;
   dispatch: AppDispatch;
 }
