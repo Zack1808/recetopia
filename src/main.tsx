c1f60bb33d31d49a66e11b8302fdbae10a80804b
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-06-07 16:56:11 +0200

    fixed bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App";
 
-import { store } from "./store";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
+  <>
     <App />
     <ToastContainer />
-  </Provider>
+  </>
 );
--- src/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export const store = configureStore({
-  reducer: {},
-});
